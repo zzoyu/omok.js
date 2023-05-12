37142ae4bcd962671d345c95117e8432ffc6492b
zzoyu <zzoyu@users.noreply.github.com>
2023-05-12 08:32:48 +0000

    [release] Publish on GitHub Pages

---
+++ assets/AboutView-247fdd59.js
@@ -0,0 +1,1 @@
+import{_ as e,b as o,e as s,f as t}from"./index-89bb6663.js";const _={},n={class:"about"},c=t("h1",null,"This is an about page",-1),a=[c];function r(i,u){return o(),s("div",n,a)}const d=e(_,[["render",r],["__file","/home/runner/work/omok.js/omok.js/src/views/AboutView.vue"]]);export{d as default};
